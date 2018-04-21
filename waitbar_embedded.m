aec06b2347067d0160738372d568f7a5d6e1b581
Mariano Aránguez <marianoai@gmail.com>
2018-04-22 01:32:14 +0200

    Squashed commit of the following:
    commit e98684f300bddf06a4db788eee6ec81c05e6443f
    Author: Mariano Aránguez <marianoai@gmail.com>
    Date:   Sat Apr 21 22:04:58 2018 +0200
    
        New: Fast Mode
    
    commit 1cb0d16d297be836c8c1a438954548e8fe3e5b1a
    Author: Mariano Aránguez <marianoai@gmail.com>
    Date:   Sat Apr 21 01:29:06 2018 +0200
    
        Changed: Drawing process improvement
    
    commit dc38a2eb6293b40c1dc11727f9e51fb665801a07
    Author: Mariano Aránguez <marianoai@gmail.com>
    Date:   Fri Apr 20 23:42:05 2018 +0200
    
        Fixed: performance improvement
    
    commit d0aaf14ca8ea6a48ebc232a3c630868f608eab71
    Author: Mariano Aránguez <marianoai@gmail.com>
    Date:   Fri Apr 20 20:02:30 2018 +0200
    
        branch for testing

--- waitbar_embedded.m
+++ waitbar_embedded.m
@@ -1,14 +1,19 @@
 function fout = waitbar_embedded(x,ax, varargin)
-%WAITBAR_EMBEDDED displays a waitbar inside to the axes given as parameter or current axes.
+%WAITBAR_EMBEDDED displays a waitbar inside to the axes given as parameter or current axes as default.
+%
 %   H = WAITBAR_EMBEDDED(X, AX, property, value, property, value, ...)
 %   creates and displays a waitbar of fractional length X.
-%   > The handle to the waitbar axes is returned in H.
-%   > X should be between 0 and 1.
-%   > AX can be supplied as a parameter by handle or unique tag.
-%   > Optional arguments property and value allow to set corresponding waitbar properties.
+%
+%   - The handle to the waitbar axes is returned in H.
+%   - X should be between 0 and 1.
+%   - AX can be supplied as a parameter by handle or unique tag.
+%   - Optional arguments property and value allow to set corresponding waitbar properties.
 %
 %   WAITBAR_EMBEDDED(X) will set the length of the bar in the most recently
 %   created axes to the fractional length X.
+%   
+%   WAITBAR_EMBEDDED([X, X_MAX]) will set the FAST MODE witch will force 
+%   just 100 steps from 0 to X_MAX
 %
 %   WAITBAR_EMBEDDED(X,AX) will set the length of the bar in axes AX
 %   to the fractional length X.mod(a,m)
@@ -24,53 +29,55 @@ function fout = waitbar_embedded(x,ax, varargin)
 %       end
 
 % Author: Mariano Aránguez
-% $Date: 2017/11/22 $
+% $Date: 2018/04/21 $ Changed: performance improvements, faster drawing. 
+%                     New: Fast Mode.
+% $Date: 2017/11/22 $ First Release
 
 %% Input parameters
-if (nargin == 0)
-    error(message('MATLAB:waitbar:InvalidArguments'));
-elseif (nargin > 0)
+if (nargin > 0)
     % Must be a numeric value
-    if ~isnumeric(x) || ~isscalar(x)
-        error(message('MATLAB:waitbar:InvalidFirstInput'));
-    elseif ((x < 0) || (x > 1))
-        % Throw a warning in this case, clamp it down and keep going
-        % this is a behavior change and we want to eventually error out in this scenario,
-        % but want to do that gradually
-        if (x < 0)
-            x = 0;
-        elseif (x > 1)
-            x = 1;
+    if isnumeric(x) && (length(x)<=2)
+        if length(x)==2 % [x, x_max] -> FAST MODE: forcing just 100 steps
+            if mod(x(1), max(1,floor(x(2)/100))) && (x(1)~=x(2))
+                return;
+            end
+            x = x(1)/x(2);
         end
-        % This warning will be enabled when callers no longer send in values
-        % outside the allowed range
-        % warning('MATLAB:waitbar:invalidValue', '%s\n%s%s', ...
-        %    'The first argument must be a numeric value between 0 and 1.',...
-        %    'Setting the value to: ', num2str(x));
+        if ((x < 0) || (x > 1))
+            if (x < 0)
+                x = 0;
+            elseif (x > 1)
+                x = 1;
+            end
+        end
+    else
+        error(message('MATLAB:waitbar:InvalidFirstInput'));
     end
     
-    if (nargin == 1)
-        % An axes handle is not provided. Look for one.
-        ax = gca;
-    elseif (nargin > 1)
+    if (nargin > 1)
         % A waitbar message or handle to an existing waitbar has been provided
         if ischar(ax) || iscellstr(ax)
             param=ax;
             ax = findobj(allchild(0), '-depth',inf, 'tag',ax);
             if isempty(ax)
-                % An axes handle is not provided. Look for one.
+                % An axes handle is not provided. Looking for one.
                 varargin = horzcat({param},varargin);
                 ax = gca;
             end
         elseif ~isgraphics(ax, 'axes')
             error('MATLAB:waitbar:InvalidInputs', 'Input arguments of type %s not valid.', class(ax))
         end
+    else
+        % An axes handle is not provided. Looking for one.
+        ax = gca;
     end
     
     if isempty(ax)
         % No axes found
         error('MATLAB:waitbar:InvalidArguments', 'No current axes found for the current figure.');
     end
+else
+    error(message('MATLAB:waitbar:InvalidArguments'));
 end
 
 %% Body
@@ -83,11 +90,11 @@ catch ex
     err = err.addCause(ex);
     throw(err);
 end
-drawnow;
 
 if nargout==1
     fout = ax;
 end
+
     %% Status Bar Drawing Function
     function drawWaitbar(varargin)
         %% axes definition
@@ -101,55 +108,31 @@ end
                 'Visible','on',...
                 'Box','on',...
                 'FontSize',10);
+            
+            % Title
             ax.Title.FontSize = 8;
             ax.Title.FontWeight = 'normal';
-            ax.UserData = struct('length',[], 'defaultFaceColor',[]);
-        end
-        
-        bDraw = true;
-        if ~isempty(ax.UserData.length)
-            bDraw = (ax.UserData.length ~= x);
-        end
-        
-        for k = 10:-1:1
-            rc{k} = findobj(allchild(ax), 'flat', 'Type','rectangle', 'Tag',sprintf('rectangle%i', k));
-        end
-        
-        %% drawing waitbar
-        if bDraw
-            ax.UserData.length = x;
             
             % scale
             px = (ax.XLim(2)-ax.XLim(1))/ax.Position(3); ox = 2*px;
             py = (ax.YLim(2)-ax.YLim(1))/ax.Position(4); oy = 2*py;
             xx = 1 - 2*ox/(ax.XLim(2)-ax.XLim(1));
             yy = 1 - 2*oy/(ax.YLim(2)-ax.YLim(1));
-
-            % rectangle definition
+           
+            % UserData initialization
+            ax.UserData = struct('length',-1, 'defaultFaceColor',zeros(1,3), 'rc',gobjects(1,10), 'text',gobjects(1,5), 'rectArea',zeros(1,4));
             for k = 10:-1:1
-                if isempty(rc{k})
-                    rc{k} = rectangle(ax, 'Tag',sprintf('rectangle%i', k), 'EdgeColor','none', 'FaceColor','g');
-                end
-                rc{k}.Position = [ox,oy,round(100*x/100)*xx,k/length(rc)*yy];
+                ax.UserData.rc(k) = rectangle(ax, 'EdgeColor','none', 'FaceColor','g');
             end
-            if isempty(ax.UserData.defaultFaceColor)
-                ax.UserData.defaultFaceColor = rc{1}.FaceColor;
-            end
-
-            % text inside the rectangle
-            txt = findobj(allchild(ax), 'flat', 'Type','text');
-            if ~isempty(txt)
-                delete(txt);
-            end
-            texto = '';
-            if x
-                texto = [num2str(round(100*x/100)),'%'];
-            end
-
+            ax.UserData.defaultFaceColor = ax.UserData.rc(1).FaceColor;
+            ax.UserData.rectArea = [ox,oy,xx,yy];
+            
             % sombra del texto
+            k=0;
             for dx=[-px px]
                 for dy=[-py py]
-                    text(ax,(ax.XLim(2) - ax.XLim(1))/2 + dx,(ax.YLim(2) - ax.YLim(1))/2 + py + dy,texto,...
+                    k=k+1;
+                    ax.UserData.text(k) = text(ax,(ax.XLim(2) - ax.XLim(1))/2 + dx,(ax.YLim(2) - ax.YLim(1))/2 + py + dy,'',...
                         'HorizontalAlignment','center',...
                         'VerticalAlignment','middle',...
                         'FontSize',ax.Position(4)/2-1,...
@@ -158,16 +141,41 @@ end
                         'Color',ax.Color);
                 end
             end
-            text(ax,(ax.XLim(2) - ax.XLim(1))/2,(ax.YLim(2) - ax.YLim(1))/2 + py,texto,...
+            % texto
+            k=k+1;
+            ax.UserData.text(k) = text(ax,(ax.XLim(2) - ax.XLim(1))/2,(ax.YLim(2) - ax.YLim(1))/2 + py,'',...
                 'HorizontalAlignment','center',...
                 'VerticalAlignment','middle',...
                 'FontSize',ax.Position(4)/2-1,...
                 'FontWeight','bold',...
                 'FontSmoothing','on',...
                 'Color',0.3*[1 1 1]);
-       end
+            
+            clear px ox py oy xx yy dx dy;
+        end
         
-        
+        bDraw = (ax.UserData.length ~= x); % optimization: checking if progress value has changed
+
+        %% drawing waitbar
+        if bDraw
+            % progress value
+            ax.UserData.length = x;
+
+            % resizing rectangles
+            for k = 10:-1:1
+                ax.UserData.rc(k).Position = [ax.UserData.rectArea(1:2), [round(100*x/100), k/length(ax.UserData.rc)].*ax.UserData.rectArea(3:4)];
+            end
+
+            % text inside the rectangle
+            texto = '';
+            if x
+                texto = [num2str(round(100*x/100)),'%'];
+            end
+            for k=1:length(ax.UserData.text)
+                ax.UserData.text(k).String=texto;
+            end
+        end
+
         %% Properties
         if nargin > 0
             % we have optional arguments: property-value pairs
@@ -177,26 +185,26 @@ end
             
             propList = varargin(1:2:end);
             valueList = varargin(2:2:end);
-
+            
             for ii = 1:length(propList)
                 try
                     if isprop(ax.Title, propList{ii})
-                        % set the prop/value pair of the axes
+                        % set the Title of the axes
                         set(ax.Title, propList{ii}, valueList{ii});
                     elseif isprop(ax, propList{ii})
                         % set the prop/value pair of the axes
                         set(ax, propList{ii}, valueList{ii});
-                    elseif isprop(rc{1}, propList{ii})
+                    elseif isprop(ax.UserData.rc(1), propList{ii})
                         % set the prop/value pair of the rectangle
                         if strcmpi(propList{ii}, 'FaceColor')
-                            set(rc{1}, propList{ii}, valueList{ii});
-                            if ~isequal(ax.UserData.defaultFaceColor, rc{1}.FaceColor)
-                                ax.UserData.defaultFaceColor = rc{1}.FaceColor;
+                            set(ax.UserData.rc(1), propList{ii}, valueList{ii});
+                            if ~isequal(ax.UserData.defaultFaceColor, ax.UserData.rc(1).FaceColor)
+                                ax.UserData.defaultFaceColor = ax.UserData.rc(1).FaceColor;
                                 bDraw = true;
                             end
                         else
-                            for k=1:length(rc)
-                                set(rc{k}, propList{ii}, valueList{ii});
+                            for k=1:length(ax.UserData.rc)
+                                set(ax.UserData.rc(k), propList{ii}, valueList{ii});
                             end
                         end
                     end
@@ -211,17 +219,18 @@ end
             % default color
             color = ax.UserData.defaultFaceColor;
             % 3D-Color
-            for k=1:length(rc)
+            for k=length(ax.UserData.rc):-1:1
                 % starting color
                 color1 = color + (1-color)*0.25;
                 % ending color
                 color2 = color * (1-0.25);
                 % rectangle color according to its height
-                heightRt = rc{k}.Position(4);
+                heightRt = ax.UserData.rc(k).Position(4);
                 colorRt = color2 + (heightRt - ax.YLim(1)) * (color1 - color2) / (ax.YLim(2)-ax.YLim(1));
-                rc{k}.FaceColor = colorRt;
+                ax.UserData.rc(k).FaceColor = colorRt;
             end
-       end
+        drawnow;
+        end
     end    
 end
 
